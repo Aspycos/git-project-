033e97e0335987f4ae0252d09c572ab050b31077
silvinoboylouis <programmerbeast296@gmail.com>
2024-12-05 11:48:42 -0800

    version 1

---
+++ javascript.js
@@ -0,0 +1,6 @@
+//const onload = document.getElementById('onload');
+//onload.addEvenlistener('')
+//const body = document.getElementById('body');
+//body.addEventListener('onload', function(){
+    alert('Hello World');
+//})
